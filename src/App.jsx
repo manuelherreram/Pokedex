1829501f44e57ff979301d24adb7ebe67fb0955d
Manuel Herrera <manuel.herrera.m@gmail.com>
2024-07-22 12:00:48 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Pokemons from "./components/Pokemons";
+import Aside from "./components/Aside";
+
+export default function App() {
+  return (
+    <section className="bg-[#F6F8FC] h-screen font-outfit overflow-y-auto">
+      <main className="max-w-[1400px] mx-auto grid grid-cols-1 lg:grid-cols-[1fr_350px] ">
+        <Pokemons />
+        <Aside />
+      </main>
+    </section>
+  );
+}
