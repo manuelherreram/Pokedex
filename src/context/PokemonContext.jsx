c99904b0e0a139907e0f284b86b72e5d91dcb017
Manuel Herrera <manuel.herrera.m@gmail.com>
2024-07-22 21:17:37 -0400

    adds PokemonContext component and user Hooks

---
+++ src/context/PokemonContext.jsx
@@ -0,0 +1,29 @@
+import { useState, createContext } from "react";
+
+const PokemonContext = createContext();
+
+const PokemonProvider = ({ children }) => {
+  const [showDetailPokemon, setShowDetailPokemon] = useState(false);
+
+  const showPokemonById = () => {
+    setShowDetailPokemon(true);
+  };
+
+  const closePokemonDetail = () => {
+    setShowDetailPokemon(false);
+  };
+
+  return (
+    <PokemonContext.Provider
+      value={{
+        showDetailPokemon,
+        showPokemonById,
+        closePokemonDetail,
+      }}
+    >
+      {children}
+    </PokemonContext.Provider>
+  );
+};
+
+export { PokemonContext, PokemonProvider };
